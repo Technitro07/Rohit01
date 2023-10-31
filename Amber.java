2b453e5dd798d07e2f810ed910ad43d70485b7b8
Technitro07 <149458371+Technitro07@users.noreply.github.com>
2023-10-31 11:54:30 +0530

    Add files via upload
    commit

---
+++ Amber.java
@@ -0,0 +1,35 @@
+class Amber
+{
+	/*indentation
+	commenting
+
+	curly breaces  #single line comment
+	multiline comment
+	execution point main()method deafault method/inbuilt method
+*/
+	
+	public static void main(String A[])
+	{
+		/*public is an Access specifier  public,private,protected,default
+		static --without creating object we can accessthe main method
+		void ---return type,every method has a return type
+		main() --method name
+		main (String args[]
+		*/
+
+		System.out.print("JSD5 Amber bath");
+
+	}
+
+}
+
+class Jsd
+{
+
+
+}
+
+class NSDC
+{
+
+}
